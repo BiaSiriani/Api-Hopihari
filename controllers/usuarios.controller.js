50a12eef8650566e6363b00da667c5d855c92d50
biasiriani <biasiriani200722@gmail.com>
2025-05-06 09:28:53 -0300

    bora

--- controllers/usuarios.controller.js
+++ controllers/usuarios.controller.js
@@ -62,9 +62,24 @@ exports.login = async () =>{
             return res.status(401).send({"Mensagem": "Usuario não cadastrado"});
         }
 
-      const match = await  bcrypt.compare(usuario[0].password,req.body.password)
-      console.log(match);
-      return
+      const match = await  bcrypt.compare(usuario[0].password,req.body.password);
+      if(!match){
+        return res.status(401).send({"Mensagen":"Senha incorreta!"});
+      }
+      console.log(match, req.body.password, usuario[0].password);
+      
+      const token =jwt.sign({
+        id:usuario[0].id,
+        first_name:usuario[0].first_name,
+        last_name:usuario[0].last_name,
+        email:usuario[0].email,
+        birth_date:usuario[0].birth_date,
+        phone:usuario[0].phone
+      },"senhadojwt");
+      return res.status(200).send({
+        "Mensagem": "Usuario autenticado co Sucesso",
+        "tokem":token
+      });
 
     } catch (error){
         return res.status(500).send({ "Error" : error })
