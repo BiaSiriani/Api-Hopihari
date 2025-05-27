febf9ea45863760e529d735998af053a99035221
biasiriani <biasiriani200722@gmail.com>
2025-05-27 07:59:14 -0300

    foi

--- controllers/brinquedos.controller.js
+++ controllers/brinquedos.controller.js
@@ -1,24 +1,23 @@
 const mysql = require('../routes/mysql');
 
 
-exports.cadastroBrinquedos = async (req, res) => {
+exports.cadastroBrinquedo = async (req, res) => {
+    try {
+      console.log('Dados recebidos:', req.body);
+      const resultados = await mysql.execute(`INSERT INTO atracoes (nome, tempo_espera, status, area) VALUES (?, ?, ?, ?)`, 
+        [   req.body.nome, 
+          req.body.tempo_espera, 
+          req.body.status, 
+          req.body.area  ]);
 
-try{
-
-const resultados = await mysql.execute(`INSERT TO atracao (nome, tempo_espera, status, area) 
-    VALUES (?, ?, ?, ?)`, 
-
-    [req.body.nome, 
-        req.body.tempo_espera, 
-        req.body.status, 
-        req.body.area]);
-
-return res.status(201).send({
-    "mensagem": "Brinquedo cadastrado com sucesso",
-    "resultados": resultados
-});
-} catch  (error){
-    return res.status(500).json({ erro: "Erro interno do servidor" });
-
-}
-}
+      console.log('Resultados da query:', resultados);
+      return res.status(201).send({
+        mensagem: "Brinquedo cadastrado com sucesso",
+        resultados: resultados
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ erro: "Erro interno do servidor" });
+    }
+  };
+  
